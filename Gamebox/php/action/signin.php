1ebf230345310badabdd93d762afad32b9c0582d
EnzoNumber7 <117282665+EnzoNumber7@users.noreply.github.com>
2023-01-24 08:58:36 +0100

    Add files via upload

---
+++ Gamebox/php/action/signin.php
@@ -0,0 +1,29 @@
+<?php
+require_once "php/config/config.php"; 
+
+unset($_SESSION['success']);
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+$sql = "Select * from user where email=:email and password=MD5(:password)";
+$dataBinded=array(
+':password'=> 'fqkejqqjfoeyeiuqfhuize'.$password,
+':email'=> $email
+);
+$pre = $pdo->prepare($sql);
+$pre->execute($dataBinded);
+$user = $pre->fetch(PDO::FETCH_ASSOC);
+
+if(empty($user)){ //vérifie si le resultat est vide !
+//non connecté
+  $_SESSION['error']="Adresse Email ou Mot de Passe incorrect";
+  header('Location:signup_in.php');
+  exit();
+
+}else{
+  unset($_SESSION['error']);  
+  $_SESSION['user'] = $user; //on enregistre que l'utilisateur est connecté
+  $_SESSION['success'] = "Vous avez été connecté avec succès";
+}
+header('Location:signup_in.php');
+exit();
